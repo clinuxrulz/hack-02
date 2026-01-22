0e51c55544878cd5fdfc20c234cc26f36c3e49e8
Clinton Selke <clinuxrulz@gmail.com>
2026-01-22 21:20:26 +1000

    missing base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
+  base: "",
   plugins: [tailwindcss(), devtools(), solidPlugin()],
   server: {
     port: 3000,
