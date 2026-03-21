9536382273b3378283bf537a758f406098b1aad4
Clinton Selke <clinuxrulz@gmail.com>
2026-03-21 16:34:10 +1000

    wip

--- src/Ball.ts
+++ src/Ball.ts
@@ -1,7 +1,24 @@
+import { type Accessor, createEffect, createSignal } from "solid-js";
 import * as THREE from "three";
 
-namespace Ball {
-  export type Params = {
-    position: THREE.Vector3,
+export function Ball(params: {
+  position: Accessor<THREE.Vector3>,
+  size: Accessor<number>,
+}): {} {
+  let position = createSignal(params.position);
+  let render = (target: THREE.Object3D) => {
+    let geometry = new THREE.SphereGeometry(params.size());
+    let material = new THREE.MeshNormalMaterial();
+    let mesh = new THREE.Mesh(geometry, material);
+    createEffect(
+      position[0],
+      (position) => {
+        mesh.position.copy(position);
+      },
+    );
+  };
+  
+  return {
+    render,
   };
 }
