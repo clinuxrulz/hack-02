a6d05951d1a6e9d1c33f2276b7957db96e331adb
Clinton Selke <clinuxrulz@gmail.com>
2026-05-23 09:02:10 +1000

    wipg

--- src/audio-worklet-processor.ts
+++ src/audio-worklet-processor.ts
@@ -1,11 +1,26 @@
+import { Fn } from "./Fn";
+let fn = new Fn();
+
 class MyAudioWorkletProcessor extends AudioWorkletProcessor {
   private frequency: number;
   private phase: number;
+  private time: number = 0.0;
+  private vars: number[] | undefined = undefined;
 
   constructor() {
     super();
     this.frequency = 440;
     this.phase = 0;
+    this.port.addEventListener("message", (e) => {
+      let params = e.data.params;
+      switch (e.data.type) {
+        case "setVars": {
+          this.time = 0.0;
+          this.vars = params.vars;
+          break;
+        }
+      }
+    });
   }
 
   process(
@@ -16,11 +31,17 @@ class MyAudioWorkletProcessor extends AudioWorkletProcessor {
     let output = outputs[0];
     output.forEach((channel) => {
       for (let i = 0; i < channel.length; i++) {
+        if (this.vars !== undefined) {
+          let note = fn.evalVars(this.time, this.vars);
+          let freq = 261.63 * Math.pow(2.0, note / 12.0);
+          this.frequency = freq;
+        }
         channel[i] = Math.sin(this.phase);
         this.phase += (2 * Math.PI * this.frequency) / sampleRate;
         if (this.phase >= 2 * Math.PI) {
           this.phase -= 2 * Math.PI;
         }
+        this.time += 1.0 / sampleRate;
       }
     });
     return true;
